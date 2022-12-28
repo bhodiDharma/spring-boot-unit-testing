aef3bbe7fcd212c62a920998377e8f18614eb8b1
bhodiDharma <antpua@gmail.com>
2022-12-28 15:45:17 +0100

    Section 5
    Mocking With Mockito

---
+++ 2.20-starting-project/src/test/java/com/luv2code/component/MockAnnotationTest.java
@@ -0,0 +1,110 @@
+package com.luv2code.component;
+
+import com.luv2code.component.dao.ApplicationDao;
+import com.luv2code.component.models.CollegeStudent;
+import com.luv2code.component.models.StudentGrades;
+import com.luv2code.component.service.ApplicationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
+public class MockAnnotationTest {
+    @Autowired
+    ApplicationContext context;
+    @Autowired
+    CollegeStudent studentOne;
+    @Autowired
+    StudentGrades studentGrades;
+    //@Mock
+    @MockBean
+    private ApplicationDao applicationDao;
+    @Autowired
+    private ApplicationService applicationService;
+
+    @BeforeEach
+    public void beforeEach() {
+        studentOne.setFirstname("Eric");
+        studentOne.setLastname("Roby");
+        studentOne.setEmailAddress("myMail@address.com");
+        studentOne.setStudentGrades(studentGrades);
+    }
+
+    @Test
+    @DisplayName("When & Verify")
+    public void assertEqualsTestAddGrades() {
+        when(applicationDao.addGradeResultsForSingleClass(studentGrades.getMathGradeResults())).thenReturn(100.00);
+
+        assertEquals(100,
+                applicationService.addGradeResultsForSingleClass(studentOne.getStudentGrades().getMathGradeResults()));
+
+
+        verify(applicationDao, times(1))
+                .addGradeResultsForSingleClass(studentGrades.getMathGradeResults());
+    }
+
+    @Test
+    @DisplayName("Find Gpa")
+    public void assertEqualsTestFindGpa() {
+        when(applicationDao.findGradePointAverage(studentGrades.getMathGradeResults()))
+                .thenReturn(88.31);
+
+        assertEquals(88.31, applicationService.findGradePointAverage(studentOne
+                .getStudentGrades().getMathGradeResults()));
+    }
+
+    @Test
+    @DisplayName("Not Null")
+    public void testAssertNotNull() {
+        when(applicationDao.checkNull(studentGrades.getMathGradeResults()))
+                .thenReturn(true);
+
+        assertNotNull(applicationService.checkNull(studentOne.getStudentGrades()
+                .getMathGradeResults()), "Object should not be null");
+    }
+
+
+    @Test
+    @DisplayName("Throw Runtime Exception")
+    public void throwRuntimeError() {
+        CollegeStudent nullStudent = (CollegeStudent) context.getBean("collegeStudent");
+
+        doThrow(new RuntimeException()).when(applicationDao).checkNull(nullStudent);
+
+        assertThrows(RuntimeException.class, () -> {
+            applicationService.checkNull(nullStudent);
+        });
+
+        verify(applicationDao, times(1)).checkNull(nullStudent);
+    }
+
+    @Test
+    @DisplayName("Multiple Stubbing")
+    public void stubbingConsecutiveCalls() {
+        CollegeStudent nullStudent = (CollegeStudent) context.getBean("collegeStudent");
+
+        when(applicationDao.checkNull(nullStudent))
+                .thenThrow(new RuntimeException())
+                .thenReturn("Do not throw exception second time");
+
+        assertThrows(RuntimeException.class, () -> {
+           applicationService.checkNull(nullStudent);
+        });
+
+        assertEquals("Do not throw exception second time", applicationService.checkNull(nullStudent));
+
+        verify(applicationDao, times(2)).checkNull(nullStudent);
+    }
+
+
+
+}
