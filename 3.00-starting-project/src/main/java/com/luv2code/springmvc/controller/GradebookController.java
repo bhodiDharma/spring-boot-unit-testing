7b630d90bd27fe376e2ca614e6f1b920eb89af54
bhodiDharma <antpua@gmail.com>
2023-01-17 09:00:43 +0100

    Section 8
    Testing Spring Boot MVC Web Apps -
    MVC Controller Testing

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/controller/GradebookController.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/controller/GradebookController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springmvc.controller;
 
 import com.luv2code.springmvc.models.*;
+import com.luv2code.springmvc.repository.StudentDao;
+import com.luv2code.springmvc.service.StudentAndGradeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,9 +14,38 @@ public class GradebookController {
 	@Autowired
 	private Gradebook gradebook;
 
+	@Autowired
+	private StudentAndGradeService studentAndGradeService;
+
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getStudents(Model m) {
+
+		Iterable<CollegeStudent> collegeStudents = studentAndGradeService.getGradebook();
+		m.addAttribute("students", collegeStudents);
+
+		return "index";
+	}
+
+	@PostMapping(value = "/")
+	public String createStudent(@ModelAttribute("student") CollegeStudent student, Model m) {
+		studentAndGradeService.createStudent(student.getFirstname(), student.getLastname(), student.getEmailAddress());
+		Iterable<CollegeStudent> collegeStudents = studentAndGradeService.getGradebook();
+		m.addAttribute("students", collegeStudents);
+		return "index";
+	}
+
+	@GetMapping("/delete/student/{id}")
+	public String deleteStudent(@PathVariable int id, Model m) {
+
+		if(!studentAndGradeService.checkIfStudentIsNull(id)){
+			return "error";
+		}
+
+		studentAndGradeService.deleteStudent(id);
+		Iterable<CollegeStudent> collegeStudents = studentAndGradeService.getGradebook();
+		m.addAttribute("students", collegeStudents);
+
 		return "index";
 	}
 
@@ -23,5 +54,6 @@ public class GradebookController {
 		public String studentInformation(@PathVariable int id, Model m) {
 		return "studentInformation";
 		}
+
 
 }
